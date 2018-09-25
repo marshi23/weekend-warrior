7b1f213073b82f3580823d7a7c7f5d9210b28be9
Maryam Shitu <maryamshitu@gmail.com>
2018-09-24 17:12:24 -0700

    Alouette#lines_for_verse defined and Alouette#verse defined, add required spacing between lines for this method and then creat Alouette#sing method

--- 05-alouette/alouette.rb
+++ 05-alouette/alouette.rb
@@ -1,10 +1,43 @@
+PARTS = ["la tête", "le bec", "les yeux", "le cou", "les ailes", "les pattes", "la queue", "le dos"]
+
 class Alouette
   def self.lines_for_verse(verse_num)
+    lines = []
+      until verse_num < 0
+        lines << ("Et" + " " + PARTS[verse_num] + "!")
+        verse_num -= 1
+      end
+      return lines
   end
 
   def self.verse(verse_num)
+    verse = []
+     2.times do
+        verse << "Je te plumerai" + " " + PARTS[verse_num] + "."
+     end
+
+
+    lines = lines_for_verse(verse_num)
+    lines.each  do |line|
+        2.times do
+          verse << line
+        end
+     end
+
+    2.times do
+      verse << "Alouette!"
+    end
+
+    verse << "A-a-a-ah"
+
+    return verse
+
   end
 
   def self.sing
+    song = []
+    song << "Alouette, gentille alouette,/nAlouette, je te plumerai."
+
+
   end
 end
